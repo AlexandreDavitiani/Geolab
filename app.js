f33edb395d19db5abd98dabe5dd80b0ee865adca
AlexandreDavitiani <135044890+AlexandreDavitiani@users.noreply.github.com>
2024-03-27 16:15:51 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var nam = "Hi , my name is Alex";
 let num = 17;
-const mixedVar = "Nmae" + 17;
+const mixedVar = "Name" + 17;
 let name = "Alex";
 let age = 16;
 let greeting = "Hello, my name is " + name + ", I am " + age + " years old";
